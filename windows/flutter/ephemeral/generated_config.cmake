0be612892a74e5f1899d24cb88f965d4479751e4
karkisaroj <karkisaroj3012@gmail.com>
2024-06-19 21:07:57 +0545

    commites

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Android studio\\New project\\mynotes" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Android studio\\New project\\mynotes"
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Android studio\\New project\\mynotes\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Android studio\\New project\\mynotes"
+  "FLUTTER_TARGET=D:\\Android studio\\New project\\mynotes\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Android studio\\New project\\mynotes\\.dart_tool\\package_config.json"
+)
